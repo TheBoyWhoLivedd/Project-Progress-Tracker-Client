1347a1b7f5a5b7cf6242d6657bfa6ffcaedf69ef
Ronnie Ayesigwa <98583453+TheBoyWhoLivedd@users.noreply.github.com>
2024-01-01 17:44:04 +0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ import { Toaster } from "./components/ui/toaster.tsx";
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <PrimeReactProvider>
+      <PrimeReactProvider value={{ unstyled: true, pt: {} }}>
         <BrowserRouter>
           <Routes>
             <Route path="/*" element={<App />} />
